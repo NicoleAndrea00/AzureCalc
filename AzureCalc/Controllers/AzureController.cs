9f9d759a614dc9059f4a319e37c303e77fb123cc
Nicole Del Rosario <delrosan@tcd.ie>
2026-04-14 13:19:04 +0100

    Add project files.

---
+++ AzureCalc/Controllers/AzureController.cs
@@ -0,0 +1,41 @@
+﻿using AzureCalc.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AzureCalc.Controllers
+{
+   
+    public class AzureController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.InstanceSize = AzureService.InstancePrices.Keys.ToList();
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(AzureService service)
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewBag.InstanceSize = AzureService.InstancePrices.Keys.ToList();
+                return View(service);            
+            }
+
+            //Confirmation of TempData
+
+            TempData["InstanceSize"] = service.InstanceSize;
+            TempData["NumInstances"] = service.NumInstances;
+            TempData["YearlyCost"] = service.CalculateYearlyCost().ToString();
+
+            return RedirectToAction("Confirmation")
+                ;
+
+        }
+
+        public IActionResult Confirmation()
+        {
+            return View();
+        }
+    }
+}
---
+++ AzureCalc/Models/AzureService.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace AzureCalc.Models
+{
+    public class AzureService
+    {
+        [Required]
+        [DisplayName("Instance Size")]
+        public string InstanceSize { get; set; }
+
+        [Required]
+        [Range(2, int.MaxValue, ErrorMessage ="Must have at least 2 instances (Azure SLA)")]
+        [DisplayName("Number of Instances")]
+        public int NumInstances { get; set; }
+
+        //price look up 
+        public static Dictionary<string, decimal> InstancePrices = new()
+        {
+            {"Very Small" , 0.02m },
+            {"Small" , 0.08m },
+            {"Medium" , 0.16m },
+            {"Large", 0.32m },
+            {"Very Large" , 0.64m },
+            {"A6" , 0.90m },
+            {"A7" , 1.80m },
+        };
+
+        public decimal CalculateYearlyCost()
+        {
+            int daysInYear = DateTime.IsLeapYear(DateTime.Now.Year) ? 366 : 365;
+            decimal hourlyRate = InstancePrices[InstanceSize] * NumInstances;
+            return hourlyRate * 24 * daysInYear;
+        }
+    }
+}
